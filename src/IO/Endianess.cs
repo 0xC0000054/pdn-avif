d7259ef9eed823d13c9053a9a192cd614505d768
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2021-01-08 04:24:31 -0700

    Bump copyright year

--- src/IO/Endianess.cs
+++ src/IO/Endianess.cs
@@ -3,7 +3,7 @@
 // This file is part of pdn-avif, a FileType plugin for Paint.NET
 // that loads and saves AVIF images.
 //
-// Copyright (c) 2020 Nicholas Hayes
+// Copyright (c) 2020, 2021 Nicholas Hayes
 //
 // This file is licensed under the MIT License.
 // See LICENSE.txt for complete licensing and attribution information.

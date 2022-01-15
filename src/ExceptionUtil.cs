6b98e60c65c006e445d5c3b8f0c1a46a6f0640e8
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2022-01-15 01:52:37 -0700

    Mark all methods with the DoesNotReturn attribute

--- src/ExceptionUtil.cs
+++ src/ExceptionUtil.cs
@@ -11,41 +11,49 @@
 ////////////////////////////////////////////////////////////////////////
 
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace AvifFileType
 {
     internal static class ExceptionUtil
     {
+        [DoesNotReturn]
         internal static void ThrowArgumentException(string message)
         {
             throw new ArgumentException(message);
         }
 
+        [DoesNotReturn]
         public static void ThrowArgumentNullException(string paramName)
         {
             throw new ArgumentNullException(paramName);
         }
 
+        [DoesNotReturn]
         public static void ThrowArgumentOutOfRangeException(string paramName)
         {
             throw new ArgumentOutOfRangeException(paramName);
         }
 
+        [DoesNotReturn]
         public static void ThrowArgumentOutOfRangeException(string paramName, string message)
         {
             throw new ArgumentOutOfRangeException(paramName, message);
         }
 
+        [DoesNotReturn]
         public static void ThrowFormatException(string message)
         {
             throw new FormatException(message);
         }
 
+        [DoesNotReturn]
         public static void ThrowInvalidOperationException(string message)
         {
             throw new InvalidOperationException(message);
         }
 
+        [DoesNotReturn]
         internal static void ThrowObjectDisposedException(string objectName)
         {
             throw new ObjectDisposedException(objectName);

56c0144cb0e405a4d294896b095084febcaeaa5e
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2024-01-11 19:08:27 -0700

    Update copyright year

--- src/AvifNative/ChromaSubsampling.h
+++ src/AvifNative/ChromaSubsampling.h
@@ -3,7 +3,7 @@
 // This file is part of pdn-avif, a FileType plugin for Paint.NET
 // that loads and saves AVIF images.
 //
-// Copyright (c) 2020, 2021, 2022 Nicholas Hayes
+// Copyright (c) 2020, 2021, 2022, 2023, 2024 Nicholas Hayes
 //
 // This file is licensed under the MIT License.
 // See LICENSE.txt for complete licensing and attribution information.
